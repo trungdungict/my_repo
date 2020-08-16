ab9cd28c7087bc686b8ef9af944e8db41f20eb70
trungdungict <trungdungict@gmail.com>
2020-08-16 09:45:00 +0700

    test

--- groovy_pipeline.groovy
+++ groovy_pipeline.groovy
@@ -2,6 +2,11 @@ node('master') {
     stage('test') {
         echo "hello world!"
 	sh '''
+		sudo apt-get install software-properties-common		
+		sudo add-apt-repository ppa:deadsnakes/ppa
+		sudo apt update
+		sudo apt-get install python3.8
+		sudo apt install python3-pip		
 		python --version
 		pip --version
 	'''
